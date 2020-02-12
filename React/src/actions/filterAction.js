1d80cfeadee5a25da04b713cab99c8670d8f103a
Advait Kukade <advaitkukade@gmail.com>
2020-02-12 16:32:05 +0530

    Removed React Content from Node

--- React/src/actions/filterAction.js
+++
@@ -1,9 +0,0 @@
-import { SET_FILTERS } from "../constants/commonConstants";
-
-
-export function setFilters(payload) {
-    return {
-        type: SET_FILTERS,
-        payload: payload
-    }
-}
--- React/src/store/saga.js
+++
@@ -1,7 +0,0 @@
-import { call, all } from "redux-saga/effects";
-import commonSaga from "../sagas/commonSaga";
-import searchSaga from "../sagas/searchSaga";
-
-export default function* rootSaga() {
-  yield all([call(commonSaga), call(searchSaga)]);
-}
